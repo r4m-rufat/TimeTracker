6a13c699fb09fc11b06e598a3f1fff64e54247d4
Rufat Nasirov <rufet.nesirov2001@mail.ru>
2022-03-10 00:20:59 +0400

    first commit

---
+++ app/src/main/java/com/codingwithrufat/timetracker/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.codingwithrufat.timetracker.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codingwithrufat.timetracker.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/codingwithrufat/timetracker/fragments/LoginFragment.java
@@ -0,0 +1,30 @@
+package com.codingwithrufat.timetracker.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codingwithrufat.timetracker.R;
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return view;
+    }
+}
